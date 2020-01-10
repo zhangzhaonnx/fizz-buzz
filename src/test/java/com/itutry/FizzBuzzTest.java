9cacd85180ef85e86a0c64dd160e070e696bebb6
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-10 12:07:11 +0800

    should return Fizz for 3, add test

--- src/test/java/com/itutry/FizzBuzzTest.java
+++ src/test/java/com/itutry/FizzBuzzTest.java
@@ -7,11 +7,20 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
 
-  @Test  public void fizzBuzz_should_return_2_when_2() {
+  @Test  public void fizzBuzz_should_return_2_for_2() {
     FizzBuzz fizzBuzz = new FizzBuzz();
 
     String value = fizzBuzz.fizzBuzz(2);
 
     Assert.assertThat(value, is("2"));
+  }
+
+  @Test
+  public void fizzBuzz_should_return_Fizz_for_3() {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
+    String value = fizzBuzz.fizzBuzz(3);
+
+    Assert.assertThat(value, is("Fizz"));
   }
 }
