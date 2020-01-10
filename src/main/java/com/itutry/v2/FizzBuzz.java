f448b7cbdbf4261d814f60db1faed18a5f83caba
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-10 20:50:11 +0800

    v2:should return Fizz for number divisible 3, add code

--- src/main/java/com/itutry/v2/FizzBuzz.java
+++ src/main/java/com/itutry/v2/FizzBuzz.java
@@ -3,6 +3,10 @@ package com.itutry.v2;
 public class FizzBuzz {
 
   public static String handle(int i) {
+    if (i % 3 == 0) {
+      return "Fizz";
+    }
+
     return String.valueOf(i);
   }
 }
