bcf31e6046c3e2505f7061740fbb9165e4ecd5d2
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-10 21:29:02 +0800

    v2:should return BuzzWhizz for number contains 5 and 3, add test

--- src/test/java/com/itutry/v2/FizzBuzzTest.java
+++ src/test/java/com/itutry/v2/FizzBuzzTest.java
@@ -76,4 +76,11 @@ public class FizzBuzzTest {
 
     Assert.assertThat(value, is("Fizz"));
   }
+
+  @Test
+  public void handle_should_return_BuzzWhizz_for_number_contains_5_and_3() {
+    String value = FizzBuzz.handle(35);
+
+    Assert.assertThat(value, is("BuzzWhizz"));
+  }
 }
