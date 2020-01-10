605a19993588d83dafd809f2d2195c2d8e57feeb
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-10 21:01:00 +0800

    v2:should return Buzz for number divisible 5, refactor

--- src/main/java/com/itutry/v2/FizzBuzz.java
+++ src/main/java/com/itutry/v2/FizzBuzz.java
@@ -2,14 +2,18 @@ package com.itutry.v2;
 
 public class FizzBuzz {
 
-  public static String handle(int i) {
-    if (i % 3 == 0) {
+  public static String handle(int num) {
+    if (isVisible(num, 3)) {
       return "Fizz";
     }
-    if (i % 5 == 0) {
+    if (isVisible(num, 5)) {
       return "Buzz";
     }
 
-    return String.valueOf(i);
+    return String.valueOf(num);
+  }
+
+  public static boolean isVisible(int num, int divisor) {
+    return num % divisor == 0;
   }
 }
