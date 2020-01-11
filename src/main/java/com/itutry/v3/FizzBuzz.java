f80f498bc55d8613f70880bb988e4196542705c6
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-01-11 14:37:43 +0800

    v3: for number divible 3 or 5 or 7

--- src/main/java/com/itutry/v3/FizzBuzz.java
+++ src/main/java/com/itutry/v3/FizzBuzz.java
@@ -3,6 +3,15 @@ package com.itutry.v3;
 public class FizzBuzz {
 
   public static Object handle(int num) {
+    if (num % 3 == 0) {
+      return "Fizz";
+    }
+    if (num % 5 == 0) {
+      return "Buzz";
+    }
+    if (num % 7 == 0) {
+      return "Whizz";
+    }
     return String.valueOf(num);
   }
 }
--- src/test/java/com/itutry/v3/FizzBuzzTest.java
+++ src/test/java/com/itutry/v3/FizzBuzzTest.java
@@ -11,4 +11,19 @@ public class FizzBuzzTest {
   public void handle_should_return_original_for_ordinary_number() {
     Assert.assertThat(FizzBuzz.handle(2), is("2"));
   }
+
+  @Test
+  public void handle_should_return_Fizz_for_number_divisible_3() {
+    Assert.assertThat(FizzBuzz.handle(6), is("Fizz"));
+  }
+
+  @Test
+  public void handle_should_return_Buzz_for_number_divisible_5() {
+    Assert.assertThat(FizzBuzz.handle(5), is("Buzz"));
+  }
+
+  @Test
+  public void handle_should_return_Whizz_for_number_divisible_7() {
+    Assert.assertThat(FizzBuzz.handle(7), is("Whizz"));
+  }
 }
